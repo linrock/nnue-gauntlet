e766617435e1309f121dd116306494765030b4f3
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 13:33:54 +0000

    Accept multiple pgn inputs with ordo calcs

--- server/ordo_calc.sh
+++ server/ordo_calc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-pgn_file=$1
-ordo -q -J -p $pgn_file -a 0 --anchor=master --draw-auto --white-auto -s 100
+# pgn_file=$1
+# ordo -q -J -p $pgn_file -a 0 --anchor=master --draw-auto --white-auto -s 100
+
+ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- $*
