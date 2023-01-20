50c5101aa65424942c24788353c2cd101a577287
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 13:47:55 +0000

    Show stats for all tcs given an nnue name

--- server/ordo_calc.sh
+++ server/ordo_calc.sh
@@ -3,4 +3,10 @@
 # pgn_file=$1
 # ordo -q -J -p $pgn_file -a 0 --anchor=master --draw-auto --white-auto -s 100
 
-ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- $*
+if [[ $1 == *.nnue ]]; then
+  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- pgns/*$1-25k*.pgn
+  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- pgns/*$1-stc*.pgn
+  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- pgns/*$1-ltc*.pgn
+else
+  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- $*
+fi
