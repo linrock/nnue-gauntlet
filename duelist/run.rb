896754fbc3cc89e2b817a6af7e6d9b6923647fb8
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 02:49:55 +0000

    Progress getitng duelist runner working

--- duelist/run.rb
+++ duelist/run.rb
@@ -1,11 +1,17 @@
+require 'json'
+
 API_KEY = 'stsRvxw3RbM6zaI1qXwknZQQ30xQ9QtEFKZedcusTC2y5nx7'
-API_URL = 'http://localhost:6055'
+API_URL = 'http://127.0.0.1:6055'
 
 while true
-  api_response = `curl #{API_URL}/gauntlet?api_key=#{API_KEY}`
-  json_data = JSON.parse api_response 
-  unless File.exists? nn_to_test
-    `curl #{API_URL}/nn?api_key=#{API_KEY}&name=#{nn_to_test}`
+  api_response = `curl -sL #{API_URL}/match?api_key=#{API_KEY}`
+  match_data = JSON.parse api_response
+  nn_to_duel = match_data["name"]
+  unless File.exists? nn_to_duel
+    `wget "#{API_URL}/nn?api_key=#{API_KEY}&name=#{nn_to_duel}" -O #{nn_to_duel}`
   end
-  `./duel_vs_master.sh`
+  puts "Duel: #{nn_to_duel}"
+  puts `./duel_vs_master.sh #{nn_to_duel} #{match_data["tc"]}`
+  puts "sleeping..."
+  sleep 2
 end
