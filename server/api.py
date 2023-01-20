7aee498dc292ed89e67a6201cf669db179a56ad7
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 01:26:03 +0000

    Endpoint for returning duel matchups

--- server/api.py
+++ server/api.py
@@ -1,4 +1,6 @@
+from glob import glob
 import os.path
+import random
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
@@ -6,17 +8,22 @@ from pydantic import BaseModel
 app = FastAPI()
 API_KEY = 'stsRvxw3RbM6zaI1qXwknZQQ30xQ9QtEFKZedcusTC2y5nx7'
 
-@app.get('/gauntlet')
-def get_gauntlet(api_key = ''):
+@app.get('/match')
+def get_match(api_key = ''):
     if api_key != API_KEY: return {"error": "Unauthorized"}
-    return {"Hello": "World"}
+    return {
+        "name": random.choice(glob('nn/nn-*.nnue')),
+        "tc": random.choice(["25k", "stc", "ltc"])
+    }
 
 @app.get('/nn')
 def get_nn(api_key = '', name = ''):
     if api_key != API_KEY: return {"error": "Unauthorized"}
     nn_filepath = f'nn/{name}'
     if os.path.isfile(nn_filepath):
-        return FileResponse(nn_filepath, media_type='application/octet-stream')
+        return FileResponse(nn_filepath,
+            media_type='application/octet-stream',
+            filename=name)
     else:
         return {"error": "File not found"}
 
