dd79edc5ceae920d118a54c0f9927d88dadd83bd
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 01:16:56 +0000

    Bind mount local nn dir into container

--- server/docker_run.sh
+++ server/docker_run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -e PORT=6055 -p 6055:6055 -d nnue-gauntlet
+docker run \
+  -e PORT=6055 -p 6055:6055 \
+  --mount type=bind,source="$(pwd)"/nn,target=/gauntlet/nn \
+  -d nnue-gauntlet
