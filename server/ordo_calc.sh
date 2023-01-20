9a36e10f304d9dadfc943e37998c3895ae97c13e
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 13:51:30 +0000

    Simplify with a function

--- server/ordo_calc.sh
+++ server/ordo_calc.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# pgn_file=$1
-# ordo -q -J -p $pgn_file -a 0 --anchor=master --draw-auto --white-auto -s 100
+function ordo_many() {
+  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- $*
+}
 
 if [[ $1 == *.nnue ]]; then
-  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- pgns/*$1-25k*.pgn
-  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- pgns/*$1-stc*.pgn
-  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- pgns/*$1-ltc*.pgn
+  ordo_many pgns/*$1-25k*.pgn
+  ordo_many pgns/*$1-stc*.pgn
+  ordo_many pgns/*$1-ltc*.pgn
 else
-  ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- $*
+  ordo_many $*
 fi
