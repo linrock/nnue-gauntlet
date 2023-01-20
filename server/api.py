c92c22be79416ec95428d98056a73fc0702b402e
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 01:16:21 +0000

    Enable serving nnue for downloading

--- server/api.py
+++ server/api.py
@@ -1,4 +1,6 @@
+import os.path
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -6,9 +8,17 @@ API_KEY = 'stsRvxw3RbM6zaI1qXwknZQQ30xQ9QtEFKZedcusTC2y5nx7'
 
 @app.get('/gauntlet')
 def get_gauntlet(api_key = ''):
-    if api_key != API_KEY:
-        return {"error": "Unauthorized"}
+    if api_key != API_KEY: return {"error": "Unauthorized"}
     return {"Hello": "World"}
+
+@app.get('/nn')
+def get_nn(api_key = '', name = ''):
+    if api_key != API_KEY: return {"error": "Unauthorized"}
+    nn_filepath = f'nn/{name}'
+    if os.path.isfile(nn_filepath):
+        return FileResponse(nn_filepath, media_type='application/octet-stream')
+    else:
+        return {"error": "File not found"}
 
 class JsonRequestData(BaseModel):
     api_key: str = ''
@@ -16,6 +26,5 @@ class JsonRequestData(BaseModel):
 
 @app.post('/pgns')
 def post_pgns(req: JsonRequestData):
-    if api_key != API_KEY:
-        return {"error": "Unauthorized"}
+    if api_key != API_KEY: return {"error": "Unauthorized"}
     return {"success": "pgn saved"}
