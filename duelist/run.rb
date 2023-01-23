dfefbbf980a7fdffa38a04b1d4eb247340192d80
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-23 12:03:07 -0500

    Catch if nn download fails also

--- duelist/run.rb
+++ duelist/run.rb
@@ -5,15 +5,27 @@ API_URL = "http://#{ENV['GAUNTLET_SERVER_IP']}:6055"
 CONCURRENCY = (`nproc`.to_i / 16)
 
 def get_match_data
-  begin
-    api_response = `curl -sL #{API_URL}/match?api_key=#{API_KEY}`
-    match_data = JSON.parse api_response
-    nn_to_duel, tc = match_data["name"], match_data["tc"]
-  rescue
-    puts "Failed to get match data from server #{API_URL}"
-    sleep 60
-    nil
+  nn_to_duel, tc =
+    begin
+      api_response = `curl -sL #{API_URL}/match?api_key=#{API_KEY}`
+      match_data = JSON.parse api_response
+      match_data["name"], match_data["tc"]
+    rescue
+      puts "Failed to get match data from server #{API_URL}"
+      sleep 60
+      return nil
+    end
+  unless File.exists? nn_to_duel
+    begin
+      `wget "#{API_URL}/nn?api_key=#{API_KEY}&name=#{nn_to_duel}" -O #{nn_to_duel}`
+      puts `sha256sum #{nn_to_duel}`
+    rescue
+      puts "Failed to download #{nn_to_duel} from server #{API_URL}"
+      sleep 60
+      return nil
+    end
   end
+  nn_to_duel, tc
 end
 
 def upload_match_pgn(nn_to_duel, filename)
@@ -37,10 +49,6 @@ CONCURRENCY.times do
     while true
       nn_to_duel, tc = get_match_data
       next unless nn_to_duel and tc
-      unless File.exists? nn_to_duel
-        `wget "#{API_URL}/nn?api_key=#{API_KEY}&name=#{nn_to_duel}" -O #{nn_to_duel}`
-        puts `sha256sum #{nn_to_duel}`
-      end
       puts "Duel: master vs #{nn_to_duel} @ #{tc}"
       nonce = "#{Time.now.to_i}-#{(10000 + rand*10000).to_i}"
       filename = "master-vs-#{nn_to_duel}-#{tc}-#{nonce}.pgn"
