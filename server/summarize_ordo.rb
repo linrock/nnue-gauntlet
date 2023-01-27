375b9ab37be85508bc7e1acd44ae0f80e2fb7cbd
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-27 02:02:08 -0500

    Ruby script to summarize ordo results

---
+++ server/summarize_ordo.rb
@@ -0,0 +1,25 @@
+def parse_ordo_results(nnue_name, ordo_output)
+  rankings = {}
+  tc = nil
+  ordo_output.split(/\n/).each do |row|
+    if row =~ /25k nodes/
+      tc = '25k nodes'
+    elsif row =~ /STC 10\+0\.1/
+      tc = 'STC 10+0.1'
+    elsif row =~ /LTC 60\+0\.6/
+      tc = 'LTC 60+0.6'
+    elsif row.include? nnue_name
+      rating, error = row.split(/\s+/)[4, 2].map(&:to_f)
+      rankings[tc] = { rating: rating, error: error }
+    end
+  end
+  rankings
+end
+
+nnue_in_gauntlet = `ls -1 nn/nn-*.nnue | xargs -n1 basename`.strip.split(/\n/)
+nnue_in_gauntlet.each do |nnue_name|
+  puts "ordo: #{nnue_name}"
+  ordo_output = `./ordo_calc.sh #{nnue_name}`
+  pp parse_ordo_results(nnue_name, ordo_output)
+  puts
+end
