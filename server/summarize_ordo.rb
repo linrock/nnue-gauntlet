943d0eed6292a8ba73adae9b38ad0154d11bd7b0
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-27 13:15:47 -0500

    Green for positive elo, clean up with constants

--- server/summarize_ordo.rb
+++ server/summarize_ordo.rb
@@ -1,15 +1,20 @@
+require 'colorize'
 require 'terminal-table'
+
+N_25K = '25k nodes'
+STC = 'STC 10+0.1'
+LTC = 'LTC 60 + 0.6'
 
 def parse_ordo_results(nnue_name, ordo_output)
   rankings = {}
   tc = nil
   ordo_output.split(/\n/).each do |row|
     if row =~ /25k nodes/
-      tc = '25k nodes'
+      tc = N_25K
     elsif row =~ /STC 10\+0\.1/
-      tc = 'STC 10+0.1'
+      tc = STC
     elsif row =~ /LTC 60\+0\.6/
-      tc = 'LTC 60+0.6'
+      tc = LTC
     elsif row.include? nnue_name
       rating, error, points, played = row.split(/\s+/)[4, 4].map(&:to_f)
       rankings[tc] = { rating: rating, error: error, points: points, played: played }
@@ -25,26 +30,27 @@ nnue_in_gauntlet.each do |nnue_name|
   ordo_output = `./ordo_calc.sh #{nnue_name}`
   results = parse_ordo_results(nnue_name, ordo_output)
   if results
-    vstc_rating = results.dig("25k nodes", :rating)
+    vstc_rating = results.dig(N_25K, :rating)
     vstc_rating = !vstc_rating.nil? && vstc_rating > 0 ? vstc_rating.to_s.green : vstc_rating
-    stc_rating = results.dig("STC 10+0.1", :rating)
+    stc_rating = results.dig(STC, :rating)
     stc_rating = !stc_rating.nil? && stc_rating > 0 ? stc_rating.to_s.green : stc_rating
-    ltc_rating = results.dig("LTC 60+0.6", :rating)
+    ltc_rating = results.dig(LTC, :rating)
     ltc_rating = !ltc_rating.nil? && ltc_rating > 0 ? ltc_rating.to_s.green : ltc_rating
     gauntlet_results << [
       nnue_name,
-      "#{vstc_rating} +/- #{results.dig("25k nodes", :error)}",
-      results.dig("25k nodes", :played),
-      "#{stc_rating} +/- #{results.dig("STC 10+0.1", :error)}",
-      results.dig("STC 10+0.1", :played),
-      "#{ltc_rating} +/- #{results.dig("LTC 60+0.6", :error)}",
-      results.dig("LTC 60+0.6", :played),
+      "#{vstc_rating} +/- #{results.dig(N_25K, :error)}",
+      results.dig(N_25K, :played),
+      "#{stc_rating} +/- #{results.dig(STC, :error)}",
+      results.dig(STC, :played),
+      "#{ltc_rating} +/- #{results.dig(LTC, :error)}",
+      results.dig(LTC, :played),
     ]
   end
 end
 
 table = Terminal::Table.new(
-  headings: ['nnue', '25k nodes', '', 'STC 10+0.1', '', 'LTC 60+0.6'], rows: gauntlet_results
+  headings: ['nnue', N_25K, '', STC, '', LTC],
+  rows: gauntlet_results
 )
 (1..6).each {|i| table.align_column i, :right }
 puts table
