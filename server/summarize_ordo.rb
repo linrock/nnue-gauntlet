c531b79bc9ca1c889f6c76105557e63a81d14c7b
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-27 13:38:00 -0500

    Calculate ordo results for each nn concurrently

--- server/summarize_ordo.rb
+++ server/summarize_ordo.rb
@@ -23,34 +23,38 @@ def parse_ordo_results(nnue_name, ordo_output)
   rankings
 end
 
-gauntlet_results = []
+ordo_threads = []
+@gauntlet_results = []
 nnue_in_gauntlet = `ls -1 nn/nn-*.nnue | xargs -n1 basename`.strip.split(/\n/)
 nnue_in_gauntlet.each do |nnue_name|
-  puts "ordo: #{nnue_name}"
-  ordo_output = `./ordo_calc.sh #{nnue_name}`
-  results = parse_ordo_results(nnue_name, ordo_output)
-  if results
-    vstc_rating = results.dig(N_25K, :rating)
-    vstc_rating = !vstc_rating.nil? && vstc_rating > 0 ? vstc_rating.to_s.green : vstc_rating
-    stc_rating = results.dig(STC, :rating)
-    stc_rating = !stc_rating.nil? && stc_rating > 0 ? stc_rating.to_s.green : stc_rating
-    ltc_rating = results.dig(LTC, :rating)
-    ltc_rating = !ltc_rating.nil? && ltc_rating > 0 ? ltc_rating.to_s.green : ltc_rating
-    gauntlet_results << [
-      nnue_name,
-      "#{vstc_rating} +/- #{results.dig(N_25K, :error)}",
-      results.dig(N_25K, :played),
-      "#{stc_rating} +/- #{results.dig(STC, :error)}",
-      results.dig(STC, :played),
-      "#{ltc_rating} +/- #{results.dig(LTC, :error)}",
-      results.dig(LTC, :played),
-    ]
+  ordo_threads << Thread.fork do
+    # puts "ordo: #{nnue_name}"
+    ordo_output = `./ordo_calc.sh #{nnue_name}`
+    results = parse_ordo_results(nnue_name, ordo_output)
+    if results
+      vstc_rating = results.dig(N_25K, :rating)
+      vstc_rating = !vstc_rating.nil? && vstc_rating > 0 ? vstc_rating.to_s.green : vstc_rating
+      stc_rating = results.dig(STC, :rating)
+      stc_rating = !stc_rating.nil? && stc_rating > 0 ? stc_rating.to_s.green : stc_rating
+      ltc_rating = results.dig(LTC, :rating)
+      ltc_rating = !ltc_rating.nil? && ltc_rating > 0 ? ltc_rating.to_s.green : ltc_rating
+      @gauntlet_results << [
+        nnue_name,
+        "#{vstc_rating} +/- #{results.dig(N_25K, :error)}",
+        results.dig(N_25K, :played),
+        "#{stc_rating} +/- #{results.dig(STC, :error)}",
+        results.dig(STC, :played),
+        "#{ltc_rating} +/- #{results.dig(LTC, :error)}",
+        results.dig(LTC, :played),
+      ]
+    end
   end
 end
+ordo_threads.map(&:join)
 
 table = Terminal::Table.new(
   headings: ['nnue', N_25K, '', STC, '', LTC],
-  rows: gauntlet_results
+  rows: @gauntlet_results
 )
 (1..6).each {|i| table.align_column i, :right }
 puts table
