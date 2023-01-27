4a600c56f1395c4632b40b61f2495d1bda25412c
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-27 02:18:19 -0500

    Use library for table format

--- server/summarize_ordo.rb
+++ server/summarize_ordo.rb
@@ -1,3 +1,5 @@
+require 'terminal-table'
+
 def parse_ordo_results(nnue_name, ordo_output)
   rankings = {}
   tc = nil
@@ -16,10 +18,23 @@ def parse_ordo_results(nnue_name, ordo_output)
   rankings
 end
 
+gauntlet_results = []
 nnue_in_gauntlet = `ls -1 nn/nn-*.nnue | xargs -n1 basename`.strip.split(/\n/)
 nnue_in_gauntlet.each do |nnue_name|
   puts "ordo: #{nnue_name}"
   ordo_output = `./ordo_calc.sh #{nnue_name}`
-  pp parse_ordo_results(nnue_name, ordo_output)
-  puts
+  results = parse_ordo_results(nnue_name, ordo_output)
+  if results
+    gauntlet_results << [
+      nnue_name,
+      "#{results.dig("25k nodes", :rating)} +/- #{results.dig("25k nodes", :error)}",
+      "#{results.dig("STC 10+0.1", :rating)} +/- #{results.dig("STC 10+0.1", :error)}",
+      "#{results.dig("LTC 60+0.6", :rating)} +/- #{results.dig("LTC 60+0.6", :error)}",
+    ]
+  end
 end
+
+table = Terminal::Table.new(
+  headings: ['nnue', '25k nodes', 'STC 10+0.1', 'LTC 60+0.6'], rows: gauntlet_results
+)
+puts table
