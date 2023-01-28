dda3785f2b5479643a1e740027c9941ef34bcdca
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-28 02:24:58 +0000

    Update ordo calc to match new directory structure

--- server/ordo_calc.sh
+++ server/ordo_calc.sh
@@ -4,13 +4,14 @@ function ordo_many() {
   ordo -q -J -a 0 --anchor=master --draw-auto --white-auto -s 100 -- $*
 }
 
-if [[ $1 == *.nnue ]]; then
+nnue_name=$1
+if [[ $nnue_name == *.nnue ]]; then
   echo 25k nodes
-  ordo_many pgns/*$1-25k*.pgn | sed 's/^/  /'
+  ordo_many pgns/$nnue_name/*$nnue_name-25k*.pgn | sed 's/^/  /'
   echo STC 10+0.1
-  ordo_many pgns/*$1-stc*.pgn | sed 's/^/  /'
+  ordo_many pgns/$nnue_name/*$nnue_name-stc*.pgn | sed 's/^/  /'
   echo LTC 60+0.6
-  ordo_many pgns/*$1-ltc*.pgn | sed 's/^/  /'
+  ordo_many pgns/$nnue_name/*$nnue_name-ltc*.pgn | sed 's/^/  /'
 else
   ordo_many $*
 fi
